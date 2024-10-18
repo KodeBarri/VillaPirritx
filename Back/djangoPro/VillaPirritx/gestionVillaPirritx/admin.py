0c9a42a6c966e21d4c85da6b965584cb9437c559
Jonathan <jhonnysoy1@gmail.com>
2024-10-18 14:00:51 +0200

    modelos 0.0.1

--- Back/djangoPro/VillaPirritx/gestionVillaPirritx/admin.py
+++ Back/djangoPro/VillaPirritx/gestionVillaPirritx/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Animal, Evento, Protectora
 
-# Register your models here.
+admin.site.register(Animal)
+admin.site.register(Evento)
+admin.site.register(Protectora)
