46895d64b404a561068c7ccdead8ce72d0dc9215
A-Goffard <aintzagoffard@gmail.com>
2024-11-06 08:08:33 +0100

    contact form

--- Front/src/app/pages/contacto/contacto.component.ts
+++ Front/src/app/pages/contacto/contacto.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
+
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [ ContactFormComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
 
 }
+
